a9ef4d2d7dfe9396e509ab2892dc93c4bf5bba0b
Olivier DOSSMANN <olivier+git@dossmann.net>
2013-05-24 15:22:34 +0200

    [ADD] Give which package is needed for view_depends.sh to work.

--- tools/view_depends.sh
+++ tools/view_depends.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
+# view_depends.sh
+#
+# Generate a tree with dependencies from Makefly.
+#
+# requirements: graphviz
+
 dot -Tpng -odepends.png ng_depends.dot && xdg-open depends.png
