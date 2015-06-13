48bf456efad7eef07a54bf5256511cc3901b4530
Olivier DOSSMANN <olivier+git@dossmann.net>
2015-06-13 21:45:36 +0200

    [UP] Tools to create tarball from last version

--- tools/prepare-new-version.sh
+++ tools/prepare-new-version.sh
@@ -24,32 +24,38 @@ then
   mkdir "$tmp/makefly"
 fi
 
-# Copy original content to a temporary one
+# Copy original content to a TEMPORARY one
 echo "Create new branch..."
 cp -r $branch/* "$tmp/makefly" || exit 1
-# Copy makefly example configuration file to the right one
+
+# Copy CONFIG file into TEMPORARY dir
 echo "Copy makefly configuration file..."
 cp $tmp/makefly/makefly.rc.example $tmp/makefly/makefly.rc
-# Change Makefly version in code
-echo "Change makefly version in Makefile file..."
-cat $tmp/makefly/Makefile |sed -e "s#\(VERSION = \).*#\1$2#g" > "$tmp/makefly/Makefile.new" && mv -f "$tmp/makefly/Makefile.new" "$tmp/makefly/Makefile"
+
+# Change Makefly version
+echo "Change makefly version to given one..."
+cat $tmp/makefly/config |sed -e "s#\(VERSION = \).*#\1$2#g" > "$tmp/makefly/config.new" && mv -f "$tmp/makefly/config.new" "$tmp/makefly/config"
+# same in Changelog file
 echo -e "\tin Changelog..."
 cat $tmp/makefly/Changelog |sed -e "s#^\(Current \).*#Version $2#g" > "$tmp/makefly/Changelog.new" && mv -f "$tmp/makefly/Changelog.new" "$tmp/makefly/Changelog"
-# Generate documentation
+
+# Generate DOCUMENTATION
 echo "Generate documentation..."
-MAKEOBJDIR="$tmp/makefly" conf="$tmp/makefly/makefly.rc" pmake -f "$tmp/makefly/Makefile" doc || exit 1
-# Delete useless files/directories
+cd $tmp/makefly && ./makefly doc && cd ../../ || exit 1
+
+# DELETE USELESS FILES/DIR
 echo "Delete specific files/directories..."
 cd $tmp/makefly && grep -Ev '^($|#)' ../../prepare-new-version.txt | xargs -n 1 rm -fr && cd ../../ || exit 1
 cd $tmp/makefly/lang && rm -f *.po && cd ../../../ || exit 1
-# Create a tarball
+
+# Create a compressed TARBALL
 echo "Create default archive..."
 cd $tmp && zip -q -r ../makefly_$2.zip makefly && cd ../ || exit 1
 
-# Create same version in french
+# FRENCH VERSION
 echo "Create french archive..."
 cat $tmp/makefly/makefly.rc.fr.example > $tmp/makefly/makefly.rc
-cd $tmp/makefly && rm -f README.md && ln -sv doc/README.fr.md README.md &&cd ../../ || exit 1
+cd $tmp/makefly && rm -f README.md && ln -sv doc/README.fr.md README.md && cd ../../ || exit 1
 cd $tmp && zip -q -r ../makefly_$2_fr.zip makefly && cd ../ || exit 1
 
 # Delete temporary directory
